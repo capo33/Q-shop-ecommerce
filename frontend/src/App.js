4ab56eaeb2b7ea9224e7e043b54c94f993bba8b1
Mohamed <s2000588@edu.bc.fi>
2023-05-23 23:01:25 +0300

    add react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Lol</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+        <h1>Welcome to Q-shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Q-shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/catr'>
+                <i className='fas fa-shopping-cart mx-1'></i>
+                Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user mx-1'></i>
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
